1819968c484ca62fe6cd116d533932f85ae25d8b
Pedro Fernandes <112490400+pmcf-percona@users.noreply.github.com>
2024-05-02 15:17:49 +0100

    Update percona-select.js
    Added a new way to find and replace the bit in the URL. Removed regex as it only worked for numbered versions, lol

--- docs/js/percona-select.js
+++ docs/js/percona-select.js
@@ -8,31 +8,34 @@ if (selectBox) {
         // Add new versions here as needed
     };
 
+    function getCurrentVersionFromUrl() {
+        for (const path of Object.values(versionMap)) {
+            if (window.location.pathname.includes(path)) {
+                return path;  // Return the matching path as soon as one is found
+            }
+        }
+        return null;  // Return null if no match is found
+    }
+
     Object.keys(versionMap).forEach(version => {
         const option = document.createElement('option');
-        option.value = version;
-        option.textContent = version; // Appending the Unicode right-pointing pointer
+        option.value = versionMap[version];
+        option.textContent = version;
         selectBox.appendChild(option);
     });
 
-    // Function to extract the current version from URL
-    function getCurrentVersionFromUrl() {
-        const regex = /\/(\d+\.\d+)\//;
-        const matches = window.location.href.match(regex);
-        return matches ? matches[1] : null;
-    }
-
     // Set initial selection based on URL
-    const currentVersion = getCurrentVersionFromUrl();
-    if (currentVersion) {
-        selectBox.value = currentVersion;
+    const currentSegment = getCurrentVersionFromUrl();
+    if (currentSegment) {
+        selectBox.value = currentSegment;
     }
 
     // Add event listener for changing URL based on selection
     selectBox.addEventListener('change', function() {
         const selectedVersion = this.value;
-        if (selectedVersion !== getCurrentVersionFromUrl()) {
-            const newUrl = window.location.href.replace(/\/(\d+\.\d+)\//, versionMap[selectedVersion]);
+        const currentSegment = getCurrentVersionFromUrl();
+        if (selectedVersion !== currentSegment) { // Only redirect if the selected version is different
+            const newUrl = window.location.href.replace(currentSegment, selectedVersion);
             window.location.href = newUrl;
         }
     });
